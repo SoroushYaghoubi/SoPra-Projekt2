7d946618799d82e71aecb443e7e74dd69c7e10d9
rekik <amine.rekik@tu-dortmund.de>
2025-03-06 22:44:10 +0100

    issue #6 : add KDoc for the class History

--- src/main/kotlin/entity/History.kt
+++ src/main/kotlin/entity/History.kt
@@ -1,5 +1,14 @@
 package entity
 
+/**
+ * Represents the history of game states in a Bonsai game.
+ *
+ * This class is responsible for tracking the current position in the history
+ * and storing all game states that have occurred during the game.
+ *
+ * @property currentPosition The current position in the history .
+ * @property gameStates A list of all game states that have been recorded in the history.
+ */
 class History {
     var currentPosition = 0
     val gameStates            = mutableListOf<BonsaiGameState>()
