ba5011bf83ab012a5b92e347323c92ecadcd7e64
Soroush Yaghoubi <soroush.yag@gmail.com>
2025-03-10 12:48:39 +0100

    DON'T ASK ME

--- src/main/kotlin/util/AxialArithmetic.kt
+++ src/main/kotlin/util/AxialArithmetic.kt
@@ -8,7 +8,7 @@ val VECTOR_RIGHT        = (1 to 0)
 val VECTOR_BOTTOM_RIGHT = (0 to 1)
 val VECTOR_BOTTOM_LEFT  = (-1 to 1)
 val VECTOR_LEFT         = (-1 to 0)
-val VECTOR_TOP_LEFT     g= (0 to -1)
+val VECTOR_TOP_LEFT     = (0 to -1)
 
 /**
  * Takes an axial coordinate as a parameter and acts like an iterator. It returns a generator that yields a tile around
