e09ffe4b3ae1cc72d243ac535da235776cd2b27e
Gary Peng <pshinyao@gmail.com>
2025-03-10 15:40:41 +0100

    update network

--- src/main/kotlin/service/BonsaiNetworkClient.kt
+++ src/main/kotlin/service/BonsaiNetworkClient.kt
@@ -1,4 +1,38 @@
 package service
 
-class BonsaiNetworkClient {
+import tools.aqua.bgw.net.client.BoardGameClient
+import tools.aqua.bgw.net.common.notification.PlayerJoinedNotification
+import tools.aqua.bgw.net.common.response.CreateGameResponse
+import tools.aqua.bgw.net.common.response.JoinGameResponse
+
+
+class BonsaiNetworkClient(private val networkService: NetworkService) :
+    BoardGameClient("playerName", "host", "secret") {
+    //TODO(the parameters from BoardGameClient's constructor are not final)
+
+    override fun onCreateGameResponse(response: CreateGameResponse) {
+
+    }
+
+    override fun onJoinGameResponse(response: JoinGameResponse) {
+
+    }
+
+    override fun onPlayerJoined(notification: PlayerJoinedNotification) {
+
+    }
+
+    /**
+    fun onStartGameMessageReceived(message : StartGameMessage) {
+
+    }
+
+    fun onCultivateMessage(message : CultivateMessage) {
+
+    }
+
+    fun onMeditateMessage(message : MeditateMessage) {
+
+    }
+    */
 }
--- src/main/kotlin/service/NetworkService.kt
+++ src/main/kotlin/service/NetworkService.kt
@@ -1,3 +1,42 @@
 package service
 
-class NetworkService(private val rootService: RootService) : AbstractRefreshingService() {}
+class NetworkService(private val rootService: RootService,
+    private var connectionState : ConnectionState = ConnectionState.DISCONNECTED) {
+
+    var client : BonsaiNetworkClient? = null
+
+    fun createGame(secret : String, name : String, sessionID : String?) {
+        //TODO
+    }
+
+    fun joinGame(secret : String, name : String, sessionID : String) {
+        //TODO
+    }
+
+    /**
+    fun sendStartGameMessage(message : StartGameMessage) {
+        //TODO
+    }
+
+    fun sendMeditateMessage(message : MeditateMessage) {
+        //TODO
+    }
+
+    fun sendCultivateMessage(message : CultivateMessage) {
+        //TODO
+    }
+    */
+
+    fun updateConnectionState(newState : ConnectionState) {
+        //TODO
+    }
+
+    private fun connect(secret: String, name : String) : Boolean {
+        //TODO
+        return true
+    }
+
+    private fun disconnect() {
+        //TODO
+    }
+}
