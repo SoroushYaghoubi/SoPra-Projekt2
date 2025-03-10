7f654e3a11faa3f24f6d7b65b3dd8846b5a62059
Awat Watti <awat.watti@ee-erbenermitlung.de>
2025-03-10 12:03:12 +0100

    testContinueGameInvalid added

--- src/test/kotlin/service/ContinueGameTest.kt
+++ src/test/kotlin/service/ContinueGameTest.kt
@@ -2,22 +2,34 @@ package service
 
 import kotlin.test.*
 import entity.*
+import org.junit.jupiter.api.assertThrows
 
 /**
  * Tests if function continueGame works correctly, by checking if all gameStates are saved correct.
  * Saved game should be the same as the gameState played with
  */
 class ContinueGameTest {
+
+    @Test
+    fun testContinueGameInvalid() {
+        val rootService = RootService()
+        val gameService = GameService(rootService)
+        rootService.currentGame = null
+        val exception =
+            assertThrows<IllegalStateException> { gameService.continueGame() }
+
+        assertEquals("There is no saved gameState", exception.message)
+    }
     /**
      * Tests if saveGame and continueGame works correctly
      */
     @Test
     fun testContinueGame() {
-
         val rootService = RootService()
         val gameService = GameService(rootService)
         val playerActionService = PlayerActionService(rootService)
         val game = BonsaiGame()
+
         val player1 = Player("Tom", PlayerType.HUMAN, true)
         val player2 = Player("Tomy", PlayerType.HUMAN, true)
         val gameState1 = BonsaiGameState(player1, mutableListOf(player1, player2), 2, States.MEDITATE)
@@ -40,5 +52,4 @@ class ContinueGameTest {
         assertEquals(game.history?.currentPosition, loadedGame.history?.currentPosition)
         assertEquals(game.history?.gameStates, loadedGame.history?.gameStates)
     }
-
 }
