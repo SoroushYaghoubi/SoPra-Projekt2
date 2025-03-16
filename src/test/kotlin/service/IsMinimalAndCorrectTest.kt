e0515d09a12b5d381765faafc8d3cfc888ccf3b9
rekik <amine.rekik@tu-dortmund.de>
2025-03-16 13:41:53 +0100

    add the method IsMinimalAndCorrect and its test and the CanPlayWoodTest

--- src/test/kotlin/service/IsMinimalAndCorrectTest.kt
+++ src/test/kotlin/service/IsMinimalAndCorrectTest.kt
@@ -1,126 +1,15 @@
 package service
 
 import entity.*
-import kotlin.test.Test
-import kotlin.test.assertFails
-import kotlin.test.assertTrue
-import kotlin.test.assertFalse
+import kotlin.test.*
 
 
-class RemoveFromTreeTest {
+class IsMinimalAndCorrectTest {
 
     /**
      * test canPlaceWoodTile returns true for valid placement of leaf, wood, flower, fruit
      */
-    @Test
-    fun `test canPlaceWoodTile returns true for valid play`() {
-        val rootService = RootService()
 
-        //SETUP for game
-        val players = mutableListOf(
-            Player("Alice", PlayerType.HUMAN, true, ColorType.RED),
-            Player("Bob", PlayerType.HUMAN, true, ColorType.BLUE),
-            Player("Tomi", PlayerType.HUMAN, true, ColorType.BLACK)
-        )
-
-        val gameState = BonsaiGameState(
-            currentPlayer = players.first(),
-            players = players,
-            botSpeed = 1,
-            currentState = States.DISCARDING
-        )
-
-        //SETUP for player
-        val playerBonsaiTree = mutableMapOf(
-            (0 to 0) to Tile(null, null, TileType.WOOD), //ROOT
-            (0 to -1) to Tile(null, null, TileType.WOOD),
-            (-1 to -1) to Tile(null, null, TileType.LEAF),
-            (1 to -1) to Tile(null, null, TileType.LEAF),
-
-
-            //   (-1 to -2) to Tile(null, null, TileType.FLOWER),
-            (0 to -2) to Tile(null, null, TileType.WOOD),
-            (1 to -2) to Tile(null, null, TileType.LEAF),
-            (2 to -2) to Tile(null, null, TileType.LEAF),
-            //(3 to -2) to Tile(null, null, TileType.FLOWER),
-
-            (0 to -3) to Tile(null, null, TileType.FLOWER),
-            (1 to -3) to Tile(null, null, TileType.LEAF),
-            (2 to -3) to Tile(null, null, TileType.LEAF),
-        )
-
-        val playerCollectedCard = mutableListOf(
-            ToolCard(41),
-            MasterCard(mutableListOf(TileType.LEAF, TileType.FRUIT), 27),
-            HelperCard(TileType.LEAF, 35),
-            GrowthCard(TileType.LEAF, 3),
-            MasterCard(mutableListOf(TileType.ANY), 24)
-        )
-
-        gameState.currentPlayer.collectedCards = playerCollectedCard
-        gameState.currentPlayer.bonsaiTree = playerBonsaiTree
-
-        val game = BonsaiGame()
-        game.currentBonsaiGameState = gameState
-        rootService.currentGame = game
-
-        assertTrue(rootService.treeService.canPlayWood())
-    }
-
-    @Test
-    fun `test canPlaceWoodTile fails`() {
-        val rootService = RootService()
-
-        //SETUP for game
-        val players = mutableListOf(
-            Player("Alice", PlayerType.HUMAN, true, ColorType.RED),
-            Player("Bob", PlayerType.HUMAN, true, ColorType.BLUE),
-            Player("Tomi", PlayerType.HUMAN, true, ColorType.BLACK)
-        )
-
-        val gameState = BonsaiGameState(
-            currentPlayer = players.first(),
-            players = players,
-            botSpeed = 1,
-            currentState = States.DISCARDING
-        )
-
-        //SETUP for player
-        val playerBonsaiTree = mutableMapOf(
-            (0 to 0) to Tile(null, null, TileType.WOOD), //ROOT
-            (0 to -1) to Tile(null, null, TileType.WOOD),
-            (-1 to -1) to Tile(null, null, TileType.LEAF),
-            (1 to -1) to Tile(null, null, TileType.LEAF),
-
-
-            (-1 to -2) to Tile(null, null, TileType.FLOWER),
-            (0 to -2) to Tile(null, null, TileType.WOOD),
-            (1 to -2) to Tile(null, null, TileType.LEAF),
-            (2 to -2) to Tile(null, null, TileType.LEAF),
-            //(3 to -2) to Tile(null, null, TileType.FLOWER),
-
-            (0 to -3) to Tile(null, null, TileType.FLOWER),
-            (1 to -3) to Tile(null, null, TileType.LEAF),
-            (2 to -3) to Tile(null, null, TileType.LEAF),
-        )
-
-        val playerCollectedCard = mutableListOf(
-            ToolCard(41),
-            MasterCard(mutableListOf(TileType.LEAF, TileType.FRUIT), 27),
-            HelperCard(TileType.LEAF, 35),
-            GrowthCard(TileType.LEAF, 3),
-            MasterCard(mutableListOf(TileType.ANY), 24)
-        )
-
-        gameState.currentPlayer.collectedCards = playerCollectedCard
-        gameState.currentPlayer.bonsaiTree = playerBonsaiTree
-
-        val game = BonsaiGame()
-        game.currentBonsaiGameState = gameState
-        rootService.currentGame = game
-
-        assertFalse(rootService.treeService.canPlayWood())
-    }
     @Test
     fun `test isMinimal`() {
         val rootService = RootService()
@@ -153,7 +42,7 @@ class RemoveFromTreeTest {
             (0 to -2) to Tile(null, null, TileType.LEAF),
             (1 to -2) to Tile(null, null, TileType.WOOD),
             (2 to -2) to Tile(null, null, TileType.LEAF),
-            //(3 to -2) to Tile(null, null, TileType.FLOWER),
+            (3 to -2) to Tile(null, null, TileType.FLOWER),
 
             (-1 to -3) to Tile(null, null, TileType.LEAF),
             (0 to -3) to Tile(null, null, TileType.WOOD),
@@ -166,7 +55,7 @@ class RemoveFromTreeTest {
             (1 to -4) to Tile(null, null, TileType.WOOD),
             (2 to -4) to Tile(null, null, TileType.LEAF),
             (3 to -4) to Tile(null, null, TileType.WOOD),
-            (4 to -4) to Tile(null, null, TileType.LEAF),
+            //(4 to -4) to Tile(null, null, TileType.LEAF),
 
 
             (0 to -5) to Tile(null, null, TileType.FLOWER),
@@ -199,11 +88,93 @@ class RemoveFromTreeTest {
         val game = BonsaiGame()
         game.currentBonsaiGameState = gameState
         rootService.currentGame = game
+         assert( rootService.treeService.isMinimalAndCorrect(Pair(1, -1)) )
+         assert( rootService.treeService.isMinimalAndCorrect(Pair(-1, -1)) )
+         assert( rootService.treeService.isMinimalAndCorrect(Pair(2, -2)) )
+         assert( rootService.treeService.isMinimalAndCorrect(Pair(0, -4)) )
+         assert( rootService.treeService.isMinimalAndCorrect(Pair(4, -6)) )
+         assertFalse( rootService.treeService.isMinimalAndCorrect(Pair(-1, -3)) )
+         assertFalse( rootService.treeService.isMinimalAndCorrect(Pair(3, -3)) )
+         assertFalse( rootService.treeService.isMinimalAndCorrect(Pair(4, -5)) )
+         assertFalse( rootService.treeService.isMinimalAndCorrect(Pair(1, -6)) )
+         assertFalse( rootService.treeService.isMinimalAndCorrect(Pair(2, -6)) )
+    }
 
-        assertTrue(rootService.treeService.isMinimalRemoval(Pair(4, -4)))
-        assertTrue(rootService.treeService.isMinimalRemoval(Pair(2, -2)))
-        assertTrue(rootService.treeService.isMinimalRemoval(Pair(3, -5)))
-        assertFalse(rootService.treeService.isMinimalRemoval(Pair(4, -6)))
+    @Test
+    fun `test another time `(){
+        val rootService = RootService()
 
+        //SETUP for game
+        val players = mutableListOf(
+            Player("Alice", PlayerType.HUMAN, true, ColorType.RED),
+            Player("Bob", PlayerType.HUMAN, true, ColorType.BLUE),
+            Player("Tomi", PlayerType.HUMAN, true, ColorType.BLACK)
+        )
+
+        val gameState = BonsaiGameState(
+            currentPlayer = players.first(),
+            players = players,
+            botSpeed = 1,
+            currentState = States.DISCARDING
+        )
+        val playerBonsaiTree = mutableMapOf(
+            (0 to 0) to Tile(null, null, TileType.WOOD),
+            (-2 to -1) to Tile(null, null, TileType.FRUIT),
+            (-1 to -1) to Tile(null, null, TileType.LEAF),
+            (0 to -1) to Tile(null, null, TileType.WOOD),
+            (1 to -1) to Tile(null, null, TileType.LEAF),
+            (2 to -1) to Tile(null, null, TileType.FRUIT),
+
+
+            (-2 to -2) to Tile(null, null, TileType.FRUIT),
+            (-1 to -2) to Tile(null, null, TileType.LEAF),
+            (0 to -2) to Tile(null, null, TileType.LEAF),
+            (1 to -2) to Tile(null, null, TileType.WOOD),
+            (2 to -2) to Tile(null, null, TileType.LEAF),
+            (3 to -2) to Tile(null, null, TileType.FLOWER),
+
+            (-1 to -3) to Tile(null, null, TileType.LEAF),
+            (0 to -3) to Tile(null, null, TileType.WOOD),
+            (1 to -3) to Tile(null, null, TileType.WOOD),
+            (2 to -3) to Tile(null, null, TileType.WOOD),
+            (3 to -3) to Tile(null, null, TileType.LEAF),
+            (4 to -3) to Tile(null, null, TileType.FLOWER),
+
+            (-1 to -4) to Tile(null, null, TileType.FRUIT),
+            (0 to -4) to Tile(null, null, TileType.LEAF),
+            (1 to -4) to Tile(null, null, TileType.WOOD),
+            (2 to -4) to Tile(null, null, TileType.LEAF),
+            (3 to -4) to Tile(null, null, TileType.WOOD),
+            (4 to -4) to Tile(null, null, TileType.LEAF),
+            (5 to -4) to Tile(null, null, TileType.FLOWER),
+
+
+            (0 to -5) to Tile(null, null, TileType.FLOWER),
+            (1 to -5) to Tile(null, null, TileType.WOOD),
+            (2 to -5) to Tile(null, null, TileType.LEAF),
+            (3 to -5) to Tile(null, null, TileType.LEAF),
+            (4 to -5) to Tile(null, null, TileType.LEAF),
+            (5 to -5) to Tile(null, null, TileType.FLOWER),
+
+            (1 to -6) to Tile(null, null, TileType.LEAF),
+            (2 to -6) to Tile(null, null, TileType.LEAF),
+            (3 to -6) to Tile(null, null, TileType.FLOWER),
+            (4 to -6) to Tile(null, null, TileType.FRUIT),
+
+
+            (2 to -7) to Tile(null, null, TileType.FRUIT)
+
+        )
+        gameState.currentPlayer.bonsaiTree = playerBonsaiTree
+        val game = BonsaiGame()
+        game.currentBonsaiGameState = gameState
+        rootService.currentGame = game
+        assertEquals(true, rootService.treeService.isMinimalAndCorrect(Pair(-1, -1)) )
+        assertEquals(true, rootService.treeService.isMinimalAndCorrect(Pair(1, -1)) )
+        assertEquals(true, rootService.treeService.isMinimalAndCorrect(Pair(-1, -3)) )
+        assertEquals(true, rootService.treeService.isMinimalAndCorrect(Pair(4, -5)) )
+        assertEquals(true, rootService.treeService.isMinimalAndCorrect(Pair(1, -6)) )
+        assertEquals(true, rootService.treeService.isMinimalAndCorrect(Pair(2, -6)) )
+        assertFalse( rootService.treeService.isMinimalAndCorrect(Pair(5, -5)))
     }
 }
