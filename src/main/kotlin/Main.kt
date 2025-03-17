083a7ea1136b0566e85c6913566ce52a377058d1
Soroush Yaghoubi <soroush.yag@gmail.com>
2025-03-17 22:40:26 +0100

    cleaned comments from main

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -5,15 +5,5 @@ import util.ZenCardLoader
  * The main Function for a Bonsai board game.
  */
 fun main() {
-    /**
-    translateToolCardsFromCSV(4).forEach {
-        println(it.id)
-    }
-    */
-    //edu.udo.cs.sopra.ntf.main()
-
     BonsaiApplication().show()
-
-    //println(ZenCardLoader().readAllZenCards(2).size)
-
 }
