3be76d18710d53e1b8ad10b71b9598371d5683e9
Dennis Anton <dennis.anton@yahoo.com>
2025-03-20 10:28:54 +0100

    renouncing a goal does not remove it from the available list anymore

--- src/main/kotlin/service/PlayerActionService.kt
+++ src/main/kotlin/service/PlayerActionService.kt
@@ -382,11 +382,10 @@ class PlayerActionService(private val rootService: RootService) : AbstractRefres
                 }
             }
         } else {
-            gameState.goalTiles.removeIf { tile ->
+            gameState.goalTiles.forEach { tile ->
                 if (tile.goalTileType == goalTileType && tile.tier == tier) {
                     player.renouncedGoals.add(tile)
-                    true
-                } else false
+                }
             }
             // update message
             if (net.connectionState != ConnectionState.DISCONNECTED &&
