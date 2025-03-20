e598c2db3a52b1403a2e6355c82f505ec666aca4
Gary Peng <pshinyao@gmail.com>
2025-03-20 23:33:23 +0100

    update test

--- src/test/kotlin/service/NetWorkClientTest.kt
+++ src/test/kotlin/service/NetWorkClientTest.kt
@@ -1,12 +1,10 @@
 package service
 
-import entity.ColorType
-import entity.GoalTileType
-import entity.Player
-import entity.PlayerType
+import entity.*
 import util.SECRET_KEY
 import kotlin.test.Test
 import kotlin.test.assertEquals
+import kotlin.test.assertNotNull
 
 /**
  * Test class for [BonsaiNetworkClient]
@@ -52,5 +50,96 @@ class NetWorkClientTest {
         checkNotNull(game1)
         checkNotNull(game2)
         assertEquals(game1.zenDeck.size, game2.zenDeck.size)
+    }
+
+    /**
+     * Test if correctly received Meditate Message
+     */
+    @Test
+    fun onMeditateMessageReceived() {
+        val rootService1 = RootService()
+        val rootService2 = RootService()
+        val sessionId = (10001.. 50000).random().toString()
+        rootService1.networkService.createGame (
+            SECRET_KEY,
+            "Gary",
+            sessionId
+        )
+
+        Thread.sleep(2000)
+        rootService2.networkService.joinGame(
+            SECRET_KEY,
+            "HIHI",
+            sessionId
+        )
+        Thread.sleep(2000)
+
+        val player1 = Player(
+            "Gary", PlayerType.HUMAN, true, ColorType.RED)
+        val player2 = Player(
+            "HIHI", PlayerType.HUMAN, false, ColorType.PURPLE)
+
+        rootService1.networkService.sendStartGameMessage(
+            mutableListOf(player1,player2),
+            mutableListOf(
+                GoalTileType.BROWN, GoalTileType.PINK, GoalTileType.GREEN)
+        )
+        Thread.sleep(1000)
+
+        val game1 = rootService1.currentGame?.currentBonsaiGameState
+        val game2 = rootService2.currentGame?.currentBonsaiGameState
+        checkNotNull(game1)
+        checkNotNull(game2)
+        game1.faceUpCards[0] = ToolCard(41)
+        game2.faceUpCards[0] = ToolCard(41)
+        rootService1.playerActionService.meditate(0, null)
+        rootService1.networkService.sendMeditateMessage()
+        Thread.sleep(1000)
+        assertEquals(game1.players[0].tileCapacity, game2.players[0].tileCapacity)
+    }
+
+    /**
+     * Test if correctly received Cultivate Message
+     */
+    @Test
+    fun onCultivateMessageReceived() {
+        val rootService1 = RootService()
+        val rootService2 = RootService()
+        val sessionId = (10001.. 50000).random().toString()
+        rootService1.networkService.createGame (
+            SECRET_KEY,
+            "Gary",
+            sessionId
+        )
+
+        Thread.sleep(2000)
+        rootService2.networkService.joinGame(
+            SECRET_KEY,
+            "HIHI",
+            sessionId
+        )
+        Thread.sleep(2000)
+
+        val player1 = Player(
+            "Gary", PlayerType.HUMAN, true, ColorType.RED)
+        val player2 = Player(
+            "HIHI", PlayerType.HUMAN, false, ColorType.PURPLE)
+
+        rootService1.networkService.sendStartGameMessage(
+            mutableListOf(player1,player2),
+            mutableListOf(
+                GoalTileType.BROWN, GoalTileType.PINK, GoalTileType.GREEN)
+        )
+        Thread.sleep(1000)
+
+        val game1 = rootService1.currentGame?.currentBonsaiGameState
+        val game2 = rootService2.currentGame?.currentBonsaiGameState
+        checkNotNull(game1)
+        checkNotNull(game2)
+        rootService1.playerActionService.cultivate()
+        //rootService1.treeService.playTile(game1.currentPlayer.personalSupply.last(), (0 to -1))
+        rootService1.playerActionService.endTurn()
+        Thread.sleep(1000)
+        assertEquals(game2.currentPlayer, game2.players[1])
     }
 }
